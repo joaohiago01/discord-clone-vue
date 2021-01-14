06530c35c162b649a57020fbac1786be57a3c88f
joaohiago01 <joaohiago96@gmail.com>
2021-01-14 08:49:06 -0300

    Init Discord Clone with Vue.js

---
+++ src/components/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="grid">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  #grid {
+    display: grid;
+    height: 100vh;
+    grid-template: 71px 240px auto 240px;
+    grid-template-rows: 46px auto 52px;
+    grid-template-areas:
+      'SL SN CI CI'
+      'SL CL CD UL'
+      'SL UI CD UL';
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import './styles/main.scss'
+import Layout from './components/Layout'
+
+new Vue({
+  el: '#app',
+  render: h => h(Layout)
+})
