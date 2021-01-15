a537bdb9649422e27e2d9ad154b91759daf61740
joaohiago01 <joaohiago96@gmail.com>
2021-01-15 08:39:07 -0300

    Create components ServerList and ServerButton

---
+++ src/components/ServerList/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <ServerButton isHome />
+    <div class="separator"></div>
+    <ServerButton :mentions='3' />
+    <ServerButton hasNotification />
+    <ServerButton />
+    <ServerButton hasNotification :mentions='5' />
+    <ServerButton v-for="servers in 20" :key="servers.id" />
+  </div>
+</template>
+
+<script>
+import ServerButton from '../ServerButton'
+
+export default {
+  components: {
+    ServerButton
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  grid-area: SL;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--tertiary);
+  padding: 11px 0;
+  max-height: 100vh;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+.separator {
+  width: 32px;
+  border-bottom: solid 2px var(--quaternary);
+  margin-bottom: 8px;
+}
+</style>
